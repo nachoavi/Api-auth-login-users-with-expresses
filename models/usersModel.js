9963598db749dbdf8ab4ebc05027be151fa2db60
Luis San Martin <luis17.sanmartin@gmail.com>
2026-04-01 18:51:42 -0300

    Add mysql database connection to create user

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,16 +1,31 @@
 import data from "../data/users.json" with { type: "json" };
+import mysql from "mysql2/promise";
+import { pool } from "../config/db.js";
 import bcrypt from "bcrypt";
 
 let users = data;
 
 export class UserModel {
   static userRegister = async (user) => {
-    const isEmailYet = users.find((userYet) => userYet.email === user.email);
-    if (!isEmailYet) {
+    const isEmailYet = await pool.query("SELECT * FROM users WHERE email = ?", [
+      user.email,
+    ]);
+    if (true) {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       user.password = hashedPassword;
-      users.push(user);
-      return user;
+      const [result] = await pool.query(
+        "INSERT INTO users (id,name,email,password,userRole,created) VALUES (?,?,?,?,?,?)",
+        [
+          user.id,
+          user.name,
+          user.email,
+          user.password,
+          user.role,
+          user.createdAt,
+        ],
+      );
+
+      return { user };
     }
     return null;
   };
